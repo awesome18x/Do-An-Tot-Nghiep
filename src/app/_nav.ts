6ecb2a001fc4a70a123025e2e45fc7c7f4ec5aea
DuyNV <duynv@DuyNVs-MacBook-Pro.local>
2020-07-09 16:54:04 +0700

    Fix menu

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -135,102 +135,102 @@ export const navItems: INavData[] = [
         icon: 'icon-star'
       }]
     },
-  {
-    name: 'Buttons',
-    url: '/buttons',
-    icon: 'icon-cursor',
-    children: [
-      {
-        name: 'Buttons',
-        url: '/buttons/buttons',
-        icon: 'icon-cursor'
-      },
-      {
-        name: 'Dropdowns',
-        url: '/buttons/dropdowns',
-        icon: 'icon-cursor'
-      },
-      {
-        name: 'Brand Buttons',
-        url: '/buttons/brand-buttons',
-        icon: 'icon-cursor'
-      }
-    ]
-  },
-  {
-    name: 'Charts',
-    url: '/charts',
-    icon: 'icon-pie-chart'
-  },
-  {
-    name: 'Icons',
-    url: '/icons',
-    icon: 'icon-star',
-    children: [
-      {
-        name: 'CoreUI Icons',
-        url: '/icons/coreui-icons',
-        icon: 'icon-star',
-        badge: {
-          variant: 'success',
-          text: 'NEW'
-        }
-      },
-      {
-        name: 'Flags',
-        url: '/icons/flags',
-        icon: 'icon-star'
-      },
-      {
-        name: 'Font Awesome',
-        url: '/icons/font-awesome',
-        icon: 'icon-star',
-        badge: {
-          variant: 'secondary',
-          text: '4.7'
-        }
-      },
-      {
-        name: 'Simple Line Icons',
-        url: '/icons/simple-line-icons',
-        icon: 'icon-star'
-      }
-    ]
-  },
-  {
-    name: 'Notifications',
-    url: '/notifications',
-    icon: 'icon-bell',
-    children: [
-      {
-        name: 'Alerts',
-        url: '/notifications/alerts',
-        icon: 'icon-bell'
-      },
-      {
-        name: 'Badges',
-        url: '/notifications/badges',
-        icon: 'icon-bell'
-      },
-      {
-        name: 'Modals',
-        url: '/notifications/modals',
-        icon: 'icon-bell'
-      }
-    ]
-  },
-  {
-    name: 'Widgets',
-    url: '/widgets',
-    icon: 'icon-calculator',
-    badge: {
-      variant: 'info',
-      text: 'NEW'
-    }
-  },
-  {
-    divider: true
-  },
+  // {
+  //   name: 'Buttons',
+  //   url: '/buttons',
+  //   icon: 'icon-cursor',
+  //   children: [
+  //     {
+  //       name: 'Buttons',
+  //       url: '/buttons/buttons',
+  //       icon: 'icon-cursor'
+  //     },
+  //     {
+  //       name: 'Dropdowns',
+  //       url: '/buttons/dropdowns',
+  //       icon: 'icon-cursor'
+  //     },
+  //     {
+  //       name: 'Brand Buttons',
+  //       url: '/buttons/brand-buttons',
+  //       icon: 'icon-cursor'
+  //     }
+  //   ]
+  // },
+  // {
+  //   name: 'Charts',
+  //   url: '/charts',
+  //   icon: 'icon-pie-chart'
+  // },
+  // {
+  //   name: 'Icons',
+  //   url: '/icons',
+  //   icon: 'icon-star',
+  //   children: [
+  //     {
+  //       name: 'CoreUI Icons',
+  //       url: '/icons/coreui-icons',
+  //       icon: 'icon-star',
+  //       badge: {
+  //         variant: 'success',
+  //         text: 'NEW'
+  //       }
+  //     },
+  //     {
+  //       name: 'Flags',
+  //       url: '/icons/flags',
+  //       icon: 'icon-star'
+  //     },
+  //     {
+  //       name: 'Font Awesome',
+  //       url: '/icons/font-awesome',
+  //       icon: 'icon-star',
+  //       badge: {
+  //         variant: 'secondary',
+  //         text: '4.7'
+  //       }
+  //     },
+  //     {
+  //       name: 'Simple Line Icons',
+  //       url: '/icons/simple-line-icons',
+  //       icon: 'icon-star'
+  //     }
+  //   ]
+  // },
+  // {
+  //   name: 'Notifications',
+  //   url: '/notifications',
+  //   icon: 'icon-bell',
+  //   children: [
+  //     {
+  //       name: 'Alerts',
+  //       url: '/notifications/alerts',
+  //       icon: 'icon-bell'
+  //     },
+  //     {
+  //       name: 'Badges',
+  //       url: '/notifications/badges',
+  //       icon: 'icon-bell'
+  //     },
+  //     {
+  //       name: 'Modals',
+  //       url: '/notifications/modals',
+  //       icon: 'icon-bell'
+  //     }
+  //   ]
+  // },
+  // {
+  //   name: 'Widgets',
+  //   url: '/widgets',
+  //   icon: 'icon-calculator',
+  //   badge: {
+  //     variant: 'info',
+  //     text: 'NEW'
+  //   }
+  // },
+  // {
+  //   divider: true
+  // },
   // {
   //   title: true,
   //   name: 'Extras',
--- src/app/views/dontiepngoaitru/components/dontiep/dontiep.component.ts
+++ src/app/views/dontiepngoaitru/components/dontiep/dontiep.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PhongKham } from '../../../../models/phongkham';
+import { KhoaphongService } from '../../services/khoaphong.service';
 
 @Component({
   selector: 'app-dontiep',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DontiepComponent implements OnInit {
 
-  constructor() { }
+  phongKhams: PhongKham[] = [];
+  selectedPK: PhongKham;
+  constructor(
+    private khoaPhongService: KhoaphongService
+  ) { }
 
   ngOnInit(): void {
+    this.khoaPhongService.getAllPhongKham().subscribe((data: any) => {
+      console.log(data);
+      this.phongKhams = data.dmKhoaPhong;
+      this.selectedPK = data.dmKhoaPhong[0];
+    }, (error) => {
+      console.log(error);
+    });
+
   }
+
+  onChange() {}
 
 }
