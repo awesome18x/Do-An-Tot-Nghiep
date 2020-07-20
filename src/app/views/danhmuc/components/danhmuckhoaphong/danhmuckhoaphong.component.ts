a3bb6dfbcf2bb25a4e904683e18faa05847169c9
duynv1895 <duynv@ql6625.fr>
2020-07-21 01:27:40 +0700

    hi

--- src/app/views/danhmuc/components/danhmuckhoaphong/danhmuckhoaphong.component.ts
+++ src/app/views/danhmuc/components/danhmuckhoaphong/danhmuckhoaphong.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { PhongKham } from '../../../../models/phongkham';
-import { KhoaphongService } from '../../services/khoaphong.service';
-import { ConfirmDialogService } from '../../confirm-dialog/confirm-dialog.service';
-import { ToastrService } from 'ngx-toastr';
+
 @Component({
   selector: 'app-danhmuckhoaphong',
   templateUrl: './danhmuckhoaphong.component.html',
