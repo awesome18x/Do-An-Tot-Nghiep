b366c53efadddf1a225cbfbfd79de0c6702b5aab
duynv1895 <duynv@ql6625.fr>
2020-08-23 00:14:36 +0700

    Add define status khambenh

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -2,3 +2,13 @@ export enum LoaiKhoaPhong {
     PhongKham = 2,
     Khoa = 1
 }
+
+export enum TrangThaiKhamBenh {
+    ChoKham = 'Cho_Kham',
+    DangKham = 'Dang_Kham',
+    DaKhamXong = 'Da_Kham_Xong',
+    HuyKham = 'Huy_Kham',
+    ChuyenVien = 'Chuyen_Vien',
+    LapBenhAnNoiTru = 'Lap_Benh_An_Noi_Tru',
+    LapBenhAnNgoaiTru = 'Lap_Benh_An_Ngoai_Tru'
+}
